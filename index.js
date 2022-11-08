18da2431347b35187275f48158537518ccab6e35
HumairaIrin <humairamuntaha24@gmail.com>
2022-11-08 19:27:56 +0600

    read all services from database completed

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tgametl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db('flashyHMI').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('flashy hmi server running');
