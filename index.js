52dc517424d90f9a2ac790829e2d959a61ad275e
HumairaIrin <humairamuntaha24@gmail.com>
2022-11-10 22:33:38 +0600

    delete method added for deleting any specific review

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/myReview/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
